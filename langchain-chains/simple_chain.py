d7a80b86ca023140d2dd463dfac126106a49b8ad
Abhishek Palve <140493725+abhishek172003@users.noreply.github.com>
2025-08-07 17:42:37 +0530

    Add files via upload

---
+++ langchain-chains/simple_chain.py
@@ -0,0 +1,24 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+# | The pipe operator is used to chain the components together.
+
+result = chain.invoke({'topic':'cricket'})
+
+print(result)
+
+chain.get_graph().print_ascii()
